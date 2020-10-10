f3360aa0d37893c98f7749de96613e32cdf9e334
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-10-10 21:15:50 +0900

    add column to ticket table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_121109) do
+ActiveRecord::Schema.define(version: 2020_10_10_121317) do
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2020_10_10_121109) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tickets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "row"
+    t.string "number"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_tickets_on_order_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "name_kana"
@@ -69,4 +78,5 @@ ActiveRecord::Schema.define(version: 2020_10_10_121109) do
   add_foreign_key "orders", "users"
   add_foreign_key "stock_details", "stocks"
   add_foreign_key "stocks", "schedules"
+  add_foreign_key "tickets", "orders"
 end
