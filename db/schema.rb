2d7f33b69c1cd38dbc7f65d16dda2e6341dedf85
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-10-10 21:12:42 +0900

    add column to order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_120716) do
+ActiveRecord::Schema.define(version: 2020_10_10_121109) do
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_orders_on_stock_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "opponent", null: false
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_10_10_120716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "stocks"
+  add_foreign_key "orders", "users"
   add_foreign_key "stock_details", "stocks"
   add_foreign_key "stocks", "schedules"
 end
