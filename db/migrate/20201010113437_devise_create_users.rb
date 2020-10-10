8d480f5d2c90dc1e6b968608cd3f00092f6b69b3
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-10-10 20:55:11 +0900

    add colmun to User table

--- db/migrate/20201010113437_devise_create_users.rb
+++ db/migrate/20201010113437_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: true
+      t.string :name_kana,          null: true
+      t.string :email,              null: true
+      t.string :encrypted_password, null: true
 
       ## Recoverable
       t.string   :reset_password_token
