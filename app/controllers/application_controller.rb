2a454c799a0f11ef400501052b920c2ba6eb8844
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-10-19 23:53:01 +0900

    create basic auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  protect_from_forgery with: :exeption
+
+
+  private
+
+  def basic_auth
+    authenticate_request_with_thhp_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
