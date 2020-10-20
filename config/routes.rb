479ca810d577d3194b308f48343f57007cd2b943
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-10-20 11:38:34 +0900

    setting routing n erb to haml convert

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, contorllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get 'users/menu', to: 'users/registrations#menu'
+  end
+  resources :users, only: :show
 
   root to: "funclubs#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
