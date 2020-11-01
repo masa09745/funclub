4314f54902a039f8dfe6e42af4064c423b5e8c02
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-11-01 11:38:41 +0900

    controller actionの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def menu
   end
 
-  def new
+  def step1
+    @user = User.new
   end
 
-  def credit
+  def step2
+    session[:name] = user_params[:name]
+    session[:name_kana] = user_params[:name_kana]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+  end
+
+  def final
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :name_kana,
+      :email,
+      :password
+    )
   end
 
 
