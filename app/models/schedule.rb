0599fe78ec6cc7166bb503bf9218d7a8a1b8a133
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-12-06 22:37:32 +0900

    schedule,stock,stock_detailの親子孫関係の構築

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,6 @@
 class Schedule < ApplicationRecord
-  has_many :stocks
+  has_many :stocks, inverse_of: :schedule
+  accepts_nested_attributes_for :stocks
 
   belongs_to :team, foreign_key: 'opponent_id'
 
--- app/models/stock_detail.rb
+++ app/models/stock_detail.rb
@@ -1,3 +1,3 @@
 class StockDetail < ApplicationRecord
-  belongs_to :stock
+  belongs_to :stock, inverse_of: :stock_details
 end
