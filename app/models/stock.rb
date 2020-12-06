135fbdb2737b532a7a80aa2cfb37400d5ec8a80c
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-12-06 22:59:47 +0900

    stock_detailモデル削除、scheduleとstockの親子関係のconttoller設定

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,5 @@
 class Stock < ApplicationRecord
   belongs_to :schedule, inverse_of: :stocks
   has_many :stock_details, inverse_of: :stock
-  accepts_nested_attributes_for :stock_details
   has_many :orders
 end
--- app/models/stock_detail.rb
+++
@@ -1,3 +0,0 @@
-class StockDetail < ApplicationRecord
-  belongs_to :stock, inverse_of: :stock_details
-end
