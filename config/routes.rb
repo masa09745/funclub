154373d339f97cf67155fed5394bd53971bb158c
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-12-24 13:56:40 +0900

    管理者用の画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   resources :stocks
 
+  resources :admins
+
   root to: "funclubs#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
