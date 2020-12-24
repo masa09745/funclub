7e3510168f7579d699d5d17224d6fe3329abbdd8
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-12-24 22:55:23 +0900

    スケジュール一覧を別コンポーネント化

---
+++ app/javascript/packs/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <scheduleList></scheduleList>
+  </div>
+</template>
+
+<script>
+  import ScheduleList from './scheduleList.vue'
+
+export default {
+  components: {
+    ScheduleList
+  }
+}
+</script>
--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -1,6 +1,6 @@
 import TurbolinksAdapter from 'vue-turbolinks' 
 import Vue from 'vue/dist/vue.esm'
-import App from '../app.vue'
+import App from './components/app.vue'
 
 Vue.use(TurbolinksAdapter)
 
