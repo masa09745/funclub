12f3573b921672df410c7fe604f22d0af220bc03
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-07 02:00:28 +0900

    在庫量一覧を別コンポーネント化

--- app/javascript/packs/components/ManagementIndex.vue
+++ app/javascript/packs/components/ManagementIndex.vue
@@ -13,20 +13,15 @@
     </div>
     <div class="stockIndex" v-show="stockDetailBool">
       <h1 class="title">在庫一覧</h1>
-      <table class="stockList">
-        <tbody>
-          <tr>
-            <th>席種</th>
-            <th>金額</th>
-            <th>在庫量</th>
-          </tr>
-          <tr class="stockDetails" v-for="stock in stocks" :key="stock.id" >
-            <td class="stockGrade">{{ stock.grade }}</td>
-            <td class="stockPrice">{{ stock.price }}</td>
-            <td class="stockRemain">{{ stock.remain }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="stockIndex__text">スケジュールを追加する</div>
+      <ul class="stockList" v-if="stocks.length">
+        <div class="stockList__index">
+            <div>席種</div>
+            <div>金額</div>
+            <div>在庫量</div>
+        </div>
+        <StockList v-for="stock in stocks" :key="stock.id" :stock="stock" />
+      </ul>
     </div>
   </div>
 </template>
@@ -37,12 +32,14 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 import moment from 'moment'
 import MatchList from './MatchList'
+import StockList from './StockList'
 
 axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken()
 
 export default {
   components: {
-    MatchList
+    MatchList,
+    StockList
   },
   data() {
     return {
@@ -80,13 +77,16 @@ export default {
 .managementIndex{
   display: flex;
 }
+
 .matchIndex{
   width: 500px;
 }
+
 .matchIndex__text{
   margin: 10px 50px;
   text-align: right;
 }
+
 .matchList{
   width: 390px;
   margin: 10px auto 0;
@@ -103,18 +103,24 @@ export default {
 .stockIndex{
   width: 500px;
 }
+
+.stockIndex__text{
+  margin: 10px 50px;
+  text-align: right;
+}
+
 .stockList{
-  width: 350px;
+  width: 300px;
+  padding: 0;
   margin: 10px auto 0;
 }
-.stockGrade{
-  width: 20%
+
+.stockList__index{
+  display: flex;
+  width: 247px;
+  justify-content: space-between;
+  font-weight: bold;
 }
-.stockPrice{
-  width: 25%;
-}
-.stockRemain{
-  width: 25%;
-}
+
 
 </style>
---
+++ app/javascript/packs/components/StockList.vue
@@ -0,0 +1,34 @@
+<template>
+<li class="stockDetails">
+  <div class="stockGrade">{{ stock.grade }}</div>
+  <div class="stockPrice">{{ stock.price }}</div>
+  <div class="stockRemain">{{ stock.remain }}</div>
+</li>
+</template>
+
+<script>
+export default {
+  props: ["stock"]
+}
+</script>
+
+<style scoped>
+
+.stockDetails{
+  height: 50px;
+  display: flex;
+  margin-bottom: 10px;
+  line-height: 50px;
+}
+.stockGrade{
+  width: 100px;
+}
+.stockPrice{
+  width: 100px;
+}
+.stockRemain{
+  width: 100px;
+
+}
+
+</style>
