a04917f694c8f9603f177dd8216ed534d65a9c2b
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-07 15:27:17 +0900

    time-date-pickerやめてvue-datetime導入

--- app/javascript/packs/components/ManagementForm.vue
+++ app/javascript/packs/components/ManagementForm.vue
@@ -2,6 +2,33 @@
   <div class="managementForm">
     <div class="formIndex">
       <h1 class="title">スケジュール新規作成</h1>
+      <form>
+        <datetime v-model="schedule.match_date" type=datetime></datetime>
+      </form>
     </div>
   </div>
 </template>
+
+<script>
+import { Datetime } from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
+
+
+import axios from 'axios';
+  export default {
+    components: {
+      datetime: Datetime
+    },
+    data: function() {
+      return {
+        schedule: {
+          match_date: ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
