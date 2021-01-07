88df0d2f2a60c4094c83f863b6aab182ef5a8e18
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-07 17:05:26 +0900

    スケジュー作成フォーム完成(フロントのみ)

--- app/javascript/packs/components/ManagementForm.vue
+++ app/javascript/packs/components/ManagementForm.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="managementForm">
     <div class="formIndex">
-      <h1 class="title">スケジュール新規作成</h1>
-      <form>
-        <datetime v-model="schedule.match_date" type=datetime></datetime>
-      </form>
+      <h3 class="title">スケジュール新規作成</h3>
+      <b-form class="scheduleForm">
+        <b-form-group label="試合日時" >
+          <datetime v-model="schedule.match_date" type=datetime ></datetime>
+        </b-form-group>
+        <b-form-group label="対戦相手">
+          <b-form-select v-model="schedule.opponent" :options="opponents">
+          </b-form-select>
+        </b-form-group>
+        <b-button variant="primary">作成</b-button>
+
+      </b-form>
     </div>
   </div>
 </template>
 
 <script>
 import { Datetime } from 'vue-datetime'
-import 'vue-datetime/dist/vue-datetime.css'
-
 
 import axios from 'axios';
   export default {
@@ -22,13 +28,32 @@ import axios from 'axios';
     data: function() {
       return {
         schedule: {
-          match_date: ''
-        }
+          match_date: '',
+          opponent: 'null'
+        },
+        opponents: [{text: '対戦相手を選んでください', value: null}, 'チームA','チームB','チームC','チームD','チームE']
+      }
+    },
+    method:{
+      onSubmit(){
+
       }
     }
   }
 </script>
 
 <style scoped>
+
+.formIndex{
+  width: 500px;
+}
+.scheduleForm{
+  width: 300px;
+  margin: 15px auto 0;
+}
+.form-group{
+  margin-right: 20px;
+}
+
 
 </style>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -10,6 +10,7 @@ import App from './app.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
