d6c0cd59853e2418ffdc7a4fc04bf6fe7150be0e
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-07 17:13:35 +0900

    datetime型をdate型とtime型に分ける

--- db/migrate/20201010120000_create_schedules.rb
+++ db/migrate/20201010120000_create_schedules.rb
@@ -1,7 +1,8 @@
 class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
-      t.datetime :match_date, null: false
+      t.date :match_date, null: false
+      t.time :match_time, null: false
       t.string :opponent
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,8 @@ ActiveRecord::Schema.define(version: 2020_10_10_121944) do
   end
 
   create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "match_date", null: false
+    t.date "match_date", null: false
+    t.time "match_time", null: false
     t.string "opponent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
