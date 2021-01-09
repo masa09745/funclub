ee6c3df0bead6c9c5ee5e422e34913cb97fa7dc8
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-09 10:52:32 +0900

    スケジュール作成フォームを修正

--- app/javascript/packs/components/CreateSchedule.vue
+++ app/javascript/packs/components/CreateSchedule.vue
@@ -3,16 +3,18 @@
     <div class="formIndex">
       <h3 class="title">スケジュール新規作成</h3>
       <b-form class="scheduleForm">
-        <b-form-group label="試合日" >
-          <b-form-datepicker v-model="schedule.match_date" placeholder="試合日を選んでください" today-button reset-button close-button locale="ja"></b-form-datepicker>
-        </b-form-group>
-        <b-form-group label="開始時間" >
-          <b-form-timepicker v-model="schedule.match_time" placeholder="開始時間を入力してください" now-button reset-button locale="ja"></b-form-timepicker>
-        </b-form-group>
-        <b-form-group label="対戦相手">
-          <b-form-select v-model="schedule.opponent" :options="opponents">
-          </b-form-select>
-        </b-form-group>
+        <div class="form-row">
+          <b-form-group label="試合日" class="col">
+            <b-form-datepicker v-model="schedule.match_date" placeholder="----/--/--" :date-format-options="{year: 'numeric', month: '2-digit', day: '2-digit'}" today-button reset-button close-button locale="ja"></b-form-datepicker>
+          </b-form-group>
+          <b-form-group label="開始時間" class="col">
+            <b-form-timepicker v-model="schedule.match_time" placeholder="--:--" now-button reset-button locale="ja"></b-form-timepicker>
+          </b-form-group>
+          <b-form-group label="対戦相手" class="col">
+            <b-form-select v-model="schedule.opponent" :options="opponents">
+            </b-form-select>
+          </b-form-group>
+        </div>
         <b-button variant="primary" @click="createSchedule">作成</b-button>
       </b-form>
     </div>
@@ -33,7 +35,7 @@ export default {
         match_time: '',
         opponent: 'null'
       },
-      opponents: [{text: '対戦相手を選んでください', value: null}, 'チームA','チームB','チームC','チームD','チームE']
+      opponents: [{text: '----', value: null}, 'チームA','チームB','チームC','チームD','チームE']
     }
   },
   methods:{
@@ -56,15 +58,11 @@ export default {
 }
 
 .formIndex{
-  width: 500px;
+  width: 700px;
 }
 .scheduleForm{
-  width: 300px;
+  width: 500px;
   margin: 15px auto 0;
 }
-.form-group{
-  margin-right: 20px;
-}
-
 
 </style>
