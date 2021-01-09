054c223b93d26814816e4d0191440f4df4d67c8b
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-09 11:33:10 +0900

    vuejsでのcss崩れの修正

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -9,8 +9,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from '../router/router.js'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '../config/style.scss'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
