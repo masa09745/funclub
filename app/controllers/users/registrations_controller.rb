5cab3fd2f5f4735b6eb425472067f61f6ae1aa4a
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-15 02:42:41 +0900

    クレジットカード登録のためにcontoller action作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  require "payjp"
 
   def step1
     @user = User.new
@@ -10,13 +11,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def step3
-
   end
 
   def final
-    @user = User.new(session[:user])
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :secret_access_key)
 
+    if params[:payjp_token].present?
+      @customer = Payjp::Customer.create(
+        description: 'FunClub',
+        email: session[:user][:email],
+        card: params[:payjp_token]
+      )
+    else
+      render :step3
+    end
+
+    @user = User.new(session[:user])
+    @user.cards.build(
+      user_id: @user.id,
+      customer: @customer.id,
+      card: @customer.default_card
+    )
     if @user.save!
+      session.clear
       session[:id] = @user.id
       sign_in User.find(session[:id]) unless user_signed_in?
       session[:user].clear
