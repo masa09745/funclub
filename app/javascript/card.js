07f8dcbc8c9741973684ee0e278c5ffba7e4396b
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-01-16 10:52:15 +0900

    ユーザー登録画面、もしくはマイページのカード登録でのみjs発火

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,35 +1,36 @@
 $(document).on('turbolinks:load', function() {
+  if(document.URL.match(/step2/)||document.URL.match(/credit/)){
+    const payjp = Payjp('pk_test_0f209ec3d7c9a45a8d950a55');
 
-  const payjp = Payjp('pk_test_0f209ec3d7c9a45a8d950a55');
+    var elements = payjp.elements();
+    var numberElement = elements.create('cardNumber');
+    var expElement = elements.create('cardExpiry');
+    var cvcElement = elements.create('cardCvc');
 
-  var elements = payjp.elements();
-  var numberElement = elements.create('cardNumber');
-  var expElement = elements.create('cardExpiry');
-  var cvcElement = elements.create('cardCvc');
+    numberElement.mount('#number');
+    expElement.mount('#exp');
+    cvcElement.mount('#cvc');
 
-  numberElement.mount('#number');
-  expElement.mount('#exp');
-  cvcElement.mount('#cvc');
+    $("#token-submit").click(function(e) {
+      e.preventDefault();
+      payjp.createToken(numberElement).then(function (r) {
+        if (r.error){
+          alert(r.error.message)
+          regist_card.prop('disabled', false)
+        }
+        else{
+          $('#credit-form').append(
+            `<input type="hidden" name="payjp_token" value=${r.id}>
+              <input type="hidden" name="card_token" value=${r.card.id}>`
+            );
+            $('#credit-form')[0].submit();
+          　 //  ↑↑　ここでtype='hidden'にしてsubmitにtokenを乗せています
 
-  $("#token-submit").click(function(e) {
-    e.preventDefault();
-    payjp.createToken(numberElement).then(function (r) {
-      if (r.error){
-        alert(r.error.message)
-        regist_card.prop('disabled', false)
-      }
-      else{
-        $('#credit-form').append(
-          `<input type="hidden" name="payjp_token" value=${r.id}>
-            <input type="hidden" name="card_token" value=${r.card.id}>`
-          );
-          $('#credit-form')[0].submit();
-        　 //  ↑↑　ここでtype='hidden'にしてsubmitにtokenを乗せています
-
-          $("#number").removeAttr("name");
-          $("#cvc").removeAttr("name");
-          $("#exp").removeAttr("name");
-      };
+            $("#number").removeAttr("name");
+            $("#cvc").removeAttr("name");
+            $("#exp").removeAttr("name");
+        };
+      });
     });
-  });
+  };
 });
