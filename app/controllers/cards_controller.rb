48e7b14ce039b8b42573bd06fb422f9afe85765c
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-02-03 12:33:36 +0900

    indexに既に持っているカード情報を表示

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,11 @@
 class CardsController < ApplicationController
+  require "payjp"
+
   def index
-    @user = User.find(params[:id])
-    @cards = Card.includes(:user).all
+    @card = Card.find_by(user_id: current_user.id)
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :secret_access_key)
+    customer = Payjp::Customer.retrieve(@card.customer)
+    @card_info = customer.cards.retrieve(@card.card)
   end
 
   def new
