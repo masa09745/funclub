ecba7c958d42caced6a115b746c5f94c825c6d0d
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-03-07 02:31:33 +0900

    ログイン画面修正、不要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get 'users/sign_up/menu', to: 'users/registrations#menu'
     get 'users/sign_up/step1', to: 'users/registrations#step1'
     get 'users/sign_up/step2', to: 'users/registrations#step2'
     get 'users/sign_up/step3', to: 'users/registrations#step3'
