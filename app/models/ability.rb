d2c9f865ed2d7005514c752ed67d0a7b69fadb2b
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-03-11 10:20:57 +0900

    cancancanにrails_admin追加、ルーティング設定、rails_admin日本語化

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    can :read,  :all
+
     if user.admin?
+      can :access, :rails_admin
       can :manage, :all
     end
+
   end
 end
