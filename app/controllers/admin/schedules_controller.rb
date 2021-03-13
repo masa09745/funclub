a7c7e4f5abfe2e9bacc92bb3488591671497fe28
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-03-14 02:17:20 +0900

    スケジュール追加フォーム作成

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -3,6 +3,12 @@ class Admin::SchedulesController < ApplicationController
     from = Time.current.at_beginning_of_month
     to = (from + 1.month)
     @schedules = Schedule.where(match_date: from...to).order(match_date: :ASC)
+    
+  end
+
+  def new
+    @schedule = Schedule.new
+    @schedule.stocks.build
   end
 
 end
